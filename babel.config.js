e338a90cc48e0f22083790d9f63b0702c821f358
Mahmoud Fathy <mahmoud.fathy1@vodafone.com>
2024-12-12 19:54:30 +0200

    [main] adjust babel and typescript configs

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,25 @@
 module.exports = {
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: [
+          '.ios.js',
+          '.android.js',
+          '.ios.jsx',
+          '.android.jsx',
+          '.js',
+          '.jsx',
+          '.json',
+          '.ts',
+          '.tsx',
+        ],
+        root: ['.'],
+        alias: {
+          App: './App',
+        },
+      },
+    ],
+  ],
   presets: ['module:@react-native/babel-preset'],
 };
