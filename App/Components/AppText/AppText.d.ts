fab625e7f7443cb53a81332ae5f10c378c632ac2
Mahmoud Fathy <mahmoud.fathy1@vodafone.com>
2024-12-14 12:35:24 +0200

    [main] update appText

--- App/Components/AppText/AppText.d.ts
+++ App/Components/AppText/AppText.d.ts
@@ -3,9 +3,11 @@ import {TextStyle} from 'react-native';
 
 type AppTextType = {
   style?: TextStyle;
-  i18nKey: string;
+  i18nKey?: string;
   children?: ReactNode;
   translateOption?: Object;
+  isCapitalized?: boolean;
+  testID?: string;
 };
 
 export {AppTextType};
--- App/Components/AppText/AppText.tsx
+++ App/Components/AppText/AppText.tsx
@@ -1,17 +1,25 @@
 import {translate} from 'App/I18n/helper';
 import {Text} from 'react-native';
 import {AppTextType} from './AppText.d';
+import _ from 'lodash';
 
 const AppText = ({
   style,
   i18nKey,
   children,
   translateOption,
+  isCapitalized,
   ...props
 }: AppTextType) => {
   return (
-    <Text style={style} {...props}>
-      {children || translate(i18nKey, translateOption)}
+    <Text
+      style={style}
+      {...props}
+      numberOfLines={1} // Restricts the text to one line
+      ellipsizeMode="tail">
+      {children || isCapitalized
+        ? _.capitalize(translate(i18nKey, translateOption))
+        : translate(i18nKey, translateOption)}
     </Text>
   );
 };
