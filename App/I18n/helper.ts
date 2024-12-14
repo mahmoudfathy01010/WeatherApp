b14e4d8c3d7beec9af1db51a2c944b0a5d34f889
Mahmoud Fathy <mahmoud.fathy1@vodafone.com>
2024-12-14 12:39:59 +0200

    [main] update I18n translate

--- App/I18n/helper.ts
+++ App/I18n/helper.ts
@@ -1,6 +1,6 @@
 import I18n from './I18n'; // Import your i18n configuration
 
-const translate = (text: string, options: Object = {}) => {
+const translate = (text?: string, options: Object = {}) => {
   return text ? I18n.t(text, {defaultValue: text, ...options}) : '';
 };
 
